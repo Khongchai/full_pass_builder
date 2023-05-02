7ffa734c40bdc323d89095b93924e092cf52035c
khongchai <world1955@hotmail.com>
2023-05-02 16:54:20 +0700

    refined APIs

--- TODO.dart
+++ TODO.dart
@@ -1,7 +1,4 @@
 // Tests (against multiple layouts)
 // Alignment helpers to get stuff going fast.
-// Refine positioner API << doing this.
 // Documentation.
-// Test that layout thing.
-// How are we going to deal with scrolling?
 // The builder should also provide a context
